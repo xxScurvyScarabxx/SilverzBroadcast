13f5a1fabf75ea44c9911caca5efaf01a78af7e1
Silverz Playz <46418269+SilverzPlayz@users.noreply.github.com>
2019-07-13 13:14:40 +0100

    Update and rename src/SilverzPlayz/SilverzEssentials/Main.php to src/SilverzPlayz/SilverzBroadcast/Main.php

--- src/SilverzPlayz/SilverzBroadcast/Main.php
+++ src/SilverzPlayz/SilverzBroadcast/Main.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-namespace SilverzPlayz\SilverzEssentials;
+namespace SilverzPlayz\SilverzBroadcast;
 use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
